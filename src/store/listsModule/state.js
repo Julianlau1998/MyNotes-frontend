be496aacf18561e43ecd7f40e81262bfa0636b73
Julian Lau <julian.lau@aol.com>
2021-12-08 21:01:03 +0100

    changed host

--- src/store/listsModule/state.js
+++ src/store/listsModule/state.js
@@ -9,7 +9,8 @@ export default {
     data: []
   },
   err: null,
-  localhost: '/api/'
+  // localhost: '/api/'
+  localhost: 'https://mynotes-service-f7xxtlrmbq-ez.a.run.app/api/',
   // localhost: 'https://mynotes-backend-f7xxtlrmbq-ey.a.run.app/api/'
   // localhost: 'http://139.162.158.148:1323/api/'
   // localhost: 'http://192.168.178.58:1323/api/'
